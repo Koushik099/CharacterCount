f61581b04ab763658bb7d0aca030faf437b6e679
Koushik099 <kou.saha.ami@gmail.com>
2022-09-23 11:34:40 +0530

    ready to deploy

---
+++ script.js
@@ -0,0 +1,24 @@
+let inputbox = document.getElementById("input");
+let wordBtn = document.getElementById("btnword");
+let charBtn = document.getElementById("btnchar");
+let showWord = document.getElementById("showwordcount");
+let showChar = document.getElementById("showcharcount");
+
+let countWord = () => {
+  let str = inputbox.value;
+  const arr = str.split(" ");
+
+  let count = arr.filter((word) => word !== "").length;
+  showWord.textContent = `Total Word is: ${count}`;
+};
+
+let countChar = () => {
+  let str = inputbox.value;
+  const arr = str.split(" ");
+  let text = arr.join("");
+
+  showChar.textContent = `Total Characters are: ${text.length}`;
+};
+
+wordBtn.addEventListener("click", countWord);
+charBtn.addEventListener("click", countChar);
